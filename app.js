6f03bd39dad31bef9f3ac44e8d6f19aedcfdfc68
dipesh1903 <dipesh1903@gmail.com>
2017-07-29 10:38:29 +0530

    2nd commit

--- app.js
+++ app.js
@@ -20,7 +20,13 @@ request({
 	url: "https://maps.googleapis.com/maps/api/geocode/json?address=" + address,
 	json : true
 }, (error, response, body) => {
+	if(error){
+		console.log('Unable to connect to google servers');
+	}else if(body.status === 'ZERO_RESULTS'){
+		console.log('Address does not exist')
+	}else if(body.status=== 'OK'){
 	console.log(`ADDRESS: ${body.results[0].formatted_address}`);
 	console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
 	console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+}
 });
